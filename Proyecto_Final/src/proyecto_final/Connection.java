d5b866c24447e48e85c140ca84f4e8d61fdc8001
freison <freisoncastro@gmail.com>
2021-10-24 10:26:46 -0600

    DataBase

---
+++ Proyecto_Final/src/proyecto_final/Connection.java
@@ -0,0 +1,10 @@
+/***
+ * Clase Connection.
+ * Establece la conexión con la base de datos Derby.
+ * Última modificación: 24/10/21.
+ */
+package proyecto_final;
+
+public class Connection {
+    
+}
