008bec9e5608c5a66934bd3d111415651d4060a6
freison <freisoncastro@gmail.com>
2021-10-29 17:50:53 -0600

    Proyecto Class

---
+++ Proyecto_Final/src/proyecto_final/Proyecto.java
@@ -0,0 +1,83 @@
+
+package proyecto_final;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Proyecto {
+    // ATRIBUTOS DE LA CLASE.
+    private String ID;
+    private String nombre;
+    private String descripcion;
+    
+    private Connection connection = new Connection();
+    
+    // CONSTRUCTORES DE LA CLASE.
+    // Constructor sin parametros.
+    public Proyecto(){
+        this.ID = "";
+        this.nombre = "";
+        this.descripcion = "";
+    }
+    
+    // Constructor con parametros V1.
+    public Proyecto(String ID, String nombre, String descripcion){
+        this.ID = ID;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+    
+    // GETTERS Y SETTERS.
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    // METODOS DE LA CLASE.
+    public int buscarUltimoProyecto(){
+        int Id = 0;
+        
+        java.sql.Connection cn = null;
+        try{
+            cn = connection.getConnection();
+            
+            Statement stmt = cn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from Proyectos order by Id desc fetch first row only");
+            
+            while(rs.next()){
+                Id = rs.getInt("ID");
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }finally{
+            try{
+                cn.close();
+            }catch(SQLException e){
+                System.out.println(e.getMessage());
+            }
+        }
+        
+        return Id;
+    }
+}
