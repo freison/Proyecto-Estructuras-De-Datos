a49192d95271b6f7bc84cd8d4bf7220ed6670621
freison <freisoncastro@gmail.com>
2021-10-28 20:34:00 -0600

    Cleaning Comments

--- Proyecto_Final/src/vistas/FrmNuevoMiembro.java
+++ Proyecto_Final/src/vistas/FrmNuevoMiembro.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package vistas;
 
 import proyecto_final.Administrador;
 
-/**
- *
- * @author Freison
- */
 public class FrmNuevoMiembro extends javax.swing.JInternalFrame {
 
-    /**
-     * Creates new form FrmNuevoMiembro
-     */
     public FrmNuevoMiembro() {
         initComponents();
         this.llenarComboBox();
