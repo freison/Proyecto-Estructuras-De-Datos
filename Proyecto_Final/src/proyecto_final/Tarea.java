0b69e38b9125af8d686f7338f59dff392409f557
freison <freisoncastro@gmail.com>
2021-12-01 16:39:42 -0600

    Tarea Class

--- Proyecto_Final/src/proyecto_final/Tarea.java
+++ Proyecto_Final/src/proyecto_final/Tarea.java
@@ -3,8 +3,40 @@ package proyecto_final;
 
 public class Tarea {
     // ATRIBUTOS DE LA CLASE.
+    private String id;
+    private String descripcion;
+    
+    private Connection connection;
+    
+    // Constructor sin parametros.
+    public Tarea(){
+        this.id = "";
+        this.descripcion = "";
+        this.connection  = new Connection();
+    }
+    
+    // Constructor con parametros.
+    public Tarea(String id, String descripcion){
+        this.id = id;
+        this.descripcion = descripcion;
+    }
     
     // GETTERS Y SETTERS DE LA CLASE.
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
     
     // METODOS DE LA CLASE.
     
