b6fadb87c522886ebac0ad9db1aebb0f22e9374a
freison <freisoncastro@gmail.com>
2021-12-01 17:17:58 -0600

    Tarea Class Methods

--- Proyecto_Final/src/proyecto_final/Tarea.java
+++ Proyecto_Final/src/proyecto_final/Tarea.java
@@ -1,6 +1,11 @@
 
 package proyecto_final;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Tarea {
     // ATRIBUTOS DE LA CLASE.
     private String id;
@@ -39,5 +44,46 @@ public class Tarea {
     }
     
     // METODOS DE LA CLASE.
+    public void Agregar(String descripcion, int estadoTareaId, int proyectoId){
+        java.sql.Connection cn = null;
+        try{
+            cn = connection.getConnection();
+            
+            String sqlQuery = "insert into TAREAS(Descripcion, EstadoTareaId, ProyectoId) values(?, ?, ?)";
+            PreparedStatement ps = cn.prepareStatement(sqlQuery);
+            ps.setString(1, descripcion);
+            ps.setInt(2, estadoTareaId);
+            ps.setInt(3, proyectoId);
+            ps.executeQuery();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }finally{
+            try{
+                cn.close();
+            }catch(SQLException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
     
+    public void agregarDetalle(int miembroId, int tareaId){
+        java.sql.Connection cn = null;
+        try{
+            cn = connection.getConnection();
+            
+            String sqlQuery = "insert into DETALLE_TAREAS_MIEMBRO(MiembroId, TareaId) values(?, ?)";
+            PreparedStatement ps = cn.prepareStatement(query);
+            ps.setInt(1, miembroId);
+            ps.setInt(2, tareaId);
+            ps.executeUpdate();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }finally{
+            try{
+                cn.close();
+            }catch(SQLException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 } // FIN DE CLASE TAREA.
